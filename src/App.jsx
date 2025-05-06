9d2fb8432753f68048001d169b631e553d56a9ff
LaylaAlharbi <la99-88@hotmail.com>
2025-05-06 10:22:27 +0200

    Updated file: fixed bug in X feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Router from "./router/Router";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
+
 function App() {
   return (
     <div className="dark:bg-gray-dark-main">
       <Router />
+      <ToastContainer position="top-right" />
     </div>
   );
 }
